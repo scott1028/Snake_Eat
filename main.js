c80effbd458b6d8ffaa82f70764c7c2a31f61841
unknown <mic1028002@gmail.com>
2013-07-15 00:33:19 +0800

    modified:   main.js

--- main.js
+++ main.js
@@ -16,27 +16,53 @@ $(document).ready(function(){
 		backgroundColor: 'grey',
 	});
 
-	// 蛇的行為
+	// 蛇的常規行為
 	span.appendTo(main);
 	span.current_move=['+','-'];
-	span.direction=['left','top'];
+	span.direction=new Object;
 	span.moveFunc=function(e){
 		span.current_move.to=span.current_move.to || span.current_move[0];
-		span.direction.to=span.direction.to || span.direction[0];
-		var c={};c[span.direction.to]=span.current_move.to+'=1';
+		span.direction.x=span.direction.x || 'left';
+		span.direction.y=span.direction.y || 'width';
+		var c={};c[span.direction.x]=span.current_move.to+'=1';
 		span.css(c);
-		if( parseInt(span.css(span.direction.to))+parseInt(span.css('width')) >= parseInt(span.parent().css('width')) )
+		if( parseInt(span.css(span.direction.x))+parseInt(span.css(span.direction.y)) >= parseInt(span.parent().css(span.direction.y)) )
 		{
 			span.current_move.to=span.current_move[1];
-			clearInterval(span.snakeActive);
-			span.snakeActive=setInterval(span.moveFunc, 10);
+			span.snakeActive=setTimeout(span.moveFunc, 10);
 		}
-		else if( parseInt(span.css(span.direction.to))<=0 ){
+		else if( parseInt(span.css(span.direction.x))<=0 ){
 			span.current_move.to=span.current_move[0];
-			clearInterval(span.snakeActive);
-			span.snakeActive=setInterval(span.moveFunc, 10);
+			span.snakeActive=setTimeout(span.moveFunc, 10);
 		}
-	}
+		else{
+			setTimeout(span.moveFunc, 10);
+		}
+	};span.snakeActive=setTimeout(span.moveFunc, 10);
 
-	span.snakeActive=setInterval(span.moveFunc, 10);
+	// 監聽鍵盤操作
+	$(document).keydown(function(e){
+		switch(e.keyCode){
+			case 87:
+				span.direction.x='top';
+				span.direction.y='height';
+				span.current_move.to=span.current_move[1];
+				break;
+			case 83:
+				span.direction.x='top';
+				span.direction.y='height';
+				span.current_move.to=span.current_move[0];
+				break;
+			case 65:
+				span.direction.x='left';
+				span.direction.y='width';
+				span.current_move.to=span.current_move[1];
+				break;
+			case 68:
+				span.direction.x='left';
+				span.direction.y='width';
+				span.current_move.to=span.current_move[0];
+				break;
+		}
+	});
 });
